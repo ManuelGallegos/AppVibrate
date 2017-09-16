e22630a99b9f194a636ebac77304f9912a6724fd
ManuelGallegos <manuel_mty@yahoo.com>
2017-09-16 16:03:23 -0400

    Add project files.

---
+++ AppVibrate/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Phone.Devices.Notification;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppVibrate
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+    
+        }
+
+        private void Btn_Vibrate_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Phone.Devices.Notification.VibrationDevice vd = VibrationDevice.GetDefault();
+            vd.Vibrate(TimeSpan.FromMilliseconds(500));
+        }
+
+        private void Btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+           // vd.Cancel();
+        }
+    }
+}
